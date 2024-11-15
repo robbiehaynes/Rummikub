aa2857923bf0dbb5912bdbe9f59aa729d7607666
Robert Haynes <robhaynes0420@gmail.com>
2024-11-15 13:44:30 +0000

    Interum RummiGame class
    Finished RummiGame base class for the meantime until UI is more refined.

--- Rummikub/Model/RummiGame.swift
+++ Rummikub/Model/RummiGame.swift
@@ -83,4 +83,100 @@ class RummiGame: NSObject, GKMatchDelegate, GKLocalPlayerListener, ObservableObj
         }
         
     }
+    
+    func takeTurn() async {
+        
+        guard currentMatchId != nil else { return }
+        
+        do {
+            let match = try await GKTurnBasedMatch.load(withID: currentMatchId!)
+            
+            let activeParticipants = match.participants.filter { $0.status != .done }
+            
+            if activeParticipants.count < 2 {
+                // Set the match outcomes for active participants.
+                for participant in activeParticipants {
+                    participant.matchOutcome = .won
+                }
+                
+                // End the match in turn.
+                try await match.endMatchInTurn(withMatch: match.matchData!)
+                
+                // Notify the local player when the match ends.
+                youWon = true
+                // TODO: Implement custom end of match handling
+            } else {
+                // Upload move
+                // lockInMove()
+                
+//                let gameData = (encodeGameData() ?? match.matchData)!
+                let gameData = match.matchData!
+                let nextParticipants = activeParticipants.filter { $0 != match.currentParticipant }
+                
+                match.setLocalizableMessageWithKey("\(localParticipant?.player.alias ?? "A player") has made their move", arguments: nil)
+                
+                try await match.endTurn(withNextParticipants: nextParticipants, turnTimeout: GKTurnTimeoutDefault, match: gameData)
+                
+                myTurn = false
+            }
+        } catch {
+            print("Error: \(error.localizedDescription).")
+//            resetGame()
+        }
+    }
+    
+    func forfeitMatch() async {
+        // Check whether there's an ongoing match.
+        guard currentMatchId != nil else { return }
+
+        do {
+            let match = try await GKTurnBasedMatch.load(withID: currentMatchId!)
+            
+            // Forfeit the match while it's the local player's turn.
+            if myTurn {
+                
+                // Create the game data to store in Game Center.
+//                let gameData = (encodeGameData() ?? match.matchData)!
+                let gameData = match.matchData!
+                // Remove the participants who quit and the current participant.
+                let nextParticipants = match.participants.filter {
+                  ($0.status != .done) && ($0 != match.currentParticipant)
+                }
+
+                // Forfeit the match.
+                try await match.participantQuitInTurn(
+                    with: GKTurnBasedMatch.Outcome.quit,
+                    nextParticipants: nextParticipants,
+                    turnTimeout: GKTurnTimeoutDefault,
+                    match: gameData)
+                
+            } else {
+                // Forfeit the match while it's not the local player's turn.
+                try await match.participantQuitOutOfTurn(with: GKTurnBasedMatch.Outcome.quit)
+            }
+        } catch {
+            print("Error: \(error.localizedDescription).")
+        }
+    }
+    
+    func sendReminder() async {
+        // Check whether there's an ongoing match.
+        guard currentMatchId != nil else { return }
+        
+        do {
+            // Load the most recent match object from the match ID.
+            let match = try await GKTurnBasedMatch.load(withID: currentMatchId!)
+            
+            // Create an array containing the current participant.
+            let participants = match.participants.filter {
+                $0 == match.currentParticipant
+            }
+            
+            // Send a reminder to the current participant.
+            try await match.sendReminder(to: participants, localizableMessageKey: "Hurry up! You're taking ages",
+                                         arguments: [])
+        } catch {
+            print("Error: \(error.localizedDescription).")
+        }
+    }
 }
